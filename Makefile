f299f0d12c8439cc613c492444ad1ce4e353f382
Thibaut Lanier <thibaut.lanier@gmail.com>
2023-04-01 12:40:23 +0200

    Ajout des tests view

--- Makefile
+++ Makefile
@@ -12,10 +12,7 @@ all : build
 
 build : build_directory server client 
 
-test: ex_test_model ex_java_test
-
-
-
+test: ex_test_model ex_test_view ex_java_test
 
 server: build_directory
 	${CC} ${CFLAGS} ${SRC}/controller/server.c  -o ${BUILD_DIR}/server
@@ -26,37 +23,43 @@ client: build_directory copy_view
 copy_view: 
 	cp ${SRC}/view/view.cfg	${BUILD_DIR}/
 
-test: ex_java_test test_model
-
 build_directory: 
 	mkdir -p build
 
 clean:
 	rm -rf  ${BUILD_DIR} *.o
 
-
-
 #------------- creation fichiers .o --------------
 
 model.o: ${SRC}/model/model.c
 	${CC} ${CFLAGS} ${SRC}/model/model.c -c
 
-
-
-
+view.o: $(SRC)/model/view.c
+	$(CC) $(CFLAGS) $(SRC)/model/view.c -c
 
 #-------------- tests -------------------
 test_model: build_directory model.o test_model.o
 	${CC} ${CFLAGS} model.o test_model.o -o ${BUILD_DIR}/$@ 
 
-
 test_model.o: ${TEST_DIR}/test_model.c
+	${CC} ${CFLAGS} ${TEST_DIR}/test_model.c -c
+
+test_view: $(TEST_DIR)/test_view.c view.o build_directory
+	$(CC) $(CFLAGS) view.o $(TEST_DIR)/test_view.c -o $(BUILD_DIR)/$@ 
+
+ex_java_test:: java_test
 	${CC} ${CFLAGS} ${TEST_DIR}/test_model.c -c
 
 java_test: client
 	$(JC) -d $(BUILD_DIR) -cp $(BUILD_DIR) $(TEST_DIR)/*.java
 
-ex_java_test:: java_test
 	java -ea -cp $(BUILD_DIR) LancerTest $(TEST)
+
 ex_test_model: test_model
 	./${BUILD_DIR}/test_model
+
+ex_test_view: test_view
+	./$(BUILD_DIR)/test_view
+	
+
+
---
+++ test/test_view.c
@@ -0,0 +1,114 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "../src/model/view.h"
+
+void test_init_view() {
+    printf("%s", __func__);
+
+    struct view v;
+    init_view(&v, 1, 30, 20, 100, 50);
+    assert(v.id == 1);
+    assert(v.position[0] == 30);
+    assert(v.position[1] = 20);
+    assert(v.dimension[0] == 100);
+    assert(v.dimension[1] == 50);
+
+    printf("\t\tOK\n");
+}
+
+void test_get_view_id() {
+    printf("%s", __func__);
+
+    struct view v;
+    init_view(&v, 1, 30, 20, 100, 50);
+    assert(get_view_id(&v) == 1);
+
+    printf("\t\tOK\n");
+}
+
+void test_get_view_dimension() {
+    printf("%s", __func__);
+
+    struct view v;
+    init_view(&v, 1, 30, 20, 100, 50);
+    const int* dimension = get_view_dimension(&v);
+    
+    assert(v.dimension[0] == 100);
+    assert(v.dimension[1] == 50);
+    assert(dimension[0] == 100);
+    assert(dimension[1] == 50);
+
+    printf("\t\tOK\n");
+}
+
+void test_get_view_position() {
+    printf("%s", __func__);
+
+    struct view v;
+    init_view(&v, 1, 30, 20, 100, 50);
+
+    const int* position = get_view_position(&v);
+    
+    assert(v.position[0] == 30);
+    assert(v.position[1] == 20);
+    assert(position[0] == 30);
+    assert(position[1] == 20);
+
+    printf("\t\tOK\n");
+}
+
+void test_set_view_dimension() {
+    printf("%s", __func__);
+
+    struct view v;
+    init_view(&v, 1, 30, 20, 100, 50);
+    set_view_dimension(&v, 80, 120);
+
+    assert(v.dimension[0] == 80);
+    assert(v.dimension[1] == 120);
+
+    printf("\t\tOK\n");
+}
+
+void test_set_view_id() {
+    printf("%s", __func__);
+
+    struct view v;
+    init_view(&v, 1, 30, 20, 100, 50);
+    set_view_id(&v, 3);
+    assert(v.id == 3);
+
+    printf("\t\tOK\n");
+}
+
+
+void test_set_view_position() {
+    printf("%s", __func__);
+
+    struct view v;
+    init_view(&v, 1, 30, 20, 100, 50);
+    set_view_position(&v, 40, 10);
+
+    assert(v.position[0] == 40);
+    assert(v.position[1] == 10);
+
+    printf("\t\tOK\n");
+}
+
+void test_view() {
+    printf("\nTEST FILE : %s\n\n", __FILE__);
+    test_init_view();
+    test_get_view_dimension();
+    test_get_view_id();
+    test_get_view_position();
+    test_set_view_id();
+    test_set_view_position();
+    test_set_view_dimension();
+}
+
+int main(){
+    test_view();
+    return 0;
+}
